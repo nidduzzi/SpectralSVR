105754e1facd02e51db2cab89a196d2c7e765b2c
nidduzzi <74892632+nidduzzi@users.noreply.github.com>
2024-12-26 15:04:44 +0700

    change save_to_pgf, scenario 1 notebook complete

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -2,25 +2,18 @@ import matplotlib.pyplot as plt
 from typing import Literal
 
 
-def save_to_pgf(fname: str, verbosity: Literal["removed", "kept"] | None = None):
+def save_to_pgf(
+    fname: str, verbosity: Literal["removed", "kept"] | None = None, plt=plt
+):
     start_idx = fname.find(".pgf")
     assert start_idx >= 0, "invalid file name"
     tmp_prefix = "tmp_"
-    plt.savefig(tmp_prefix + fname)
-    fr = open(tmp_prefix + fname, "r")
-    fw = open(fname, "w")
-    for line in fr.readlines():
-        # reading all lines except comments
-        if not (line.startswith("%%")):
-            # printing those lines
-            # storing only those lines that
-            # do not begin with "TextGenerator"
-            fw.write(line)
-            if verbosity == "kept":
-                print(line)
-        elif verbosity == "removed":
-            print(line)
-
-    # close and save the files
-    fw.close()
-    fr.close()
+    plt.savefig(tmp_prefix + fname, transparent=True)
+    plt.savefig(fname, transparent=True)
+    with open(fname, "r+") as f:
+        lines = f.readlines()
+        f.seek(0)
+        for line in lines:
+            if not line.startswith("%%"):
+                f.write(line)
+        f.truncate()
